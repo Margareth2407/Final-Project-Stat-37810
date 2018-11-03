3359e1000423f963a1a0149a6a6a74107a99ce05
cardinalbraxiatel <43788431+cardinalbraxiatel@users.noreply.github.com>
2018-11-03 15:05:25 -0500

    Update K-means with comments
    K-means added comments
    K-means, created new function with
    sorting of the data done seperately

--- K-Means.R
+++ K-Means.R
@@ -1,46 +1,61 @@
-library(rattle)
-library(fpc)
+library(rattle) #import data
+library(fpc) #import plot
 data(wine)
 
-mywine <- as.matrix(wine[,-1])
+mywine <- as.matrix(wine[,-1]) # remove the existing clustering
 
-cluster <- function(data){
-  nrows <- length(data[,1])
-  ncols <- length(data[1,])
+cluster <- function(data){ #declare function
+  nrows <- length(data[,1]) #create variable with number of rows of data
+  ncols <- length(data[1,]) #create variable with number of columns of data
   iterations <- 20
   
-  means <- data[sample(1:nrows,3),]
-  for(i in 1:iterations){
-    distances <- matrix(rep(0,3*nrows),nrow = nrows)
-    mindistances <- matrix(rep(0, 2*nrows), nrow = nrows)
-    for(j in 1:nrows){
-      for(k in 1:3){
-        distances[j,k] <- sum((data[j,]-means[k,])^2)
+  means <- data[sample(1:nrows,3),] #select rows from the original data set to serve as the initial means/centers
+  for(i in 1:iterations){ #for loop for each of the iterations of reassigning the means
+    distances <- matrix(rep(0,3*nrows),nrow = nrows) #distances matrix initialized
+    mindistances <- matrix(rep(0, 2*nrows), nrow = nrows) #mindistances matrix initialized
+    for(j in 1:nrows){ #for each row
+      for(k in 1:3){ #for each cluster
+        distances[j,k] <- sum((data[j,]-means[k,])^2) 
+        #finds the difference between the row j and the mean ascribed to cluster K
       }
-      mindistances[j,] <- c(min(distances[j,]), which.min(distances[j,]))
+      mindistances[j,] <- c(min(distances[j,]), which.min(distances[j,])) 
+      #finds the minimum distance for each row amongst the three clusters and assigns it to mindistances
+      # in the first column and the second column has the cluster
     }
-    for(l in 1:3){
-      datarows <- data[mindistances[,2]==l,]
-      means[l,] <- (1/length(mindistances[mindistances[,2] == l,1]))*colSums(data[mindistances[,2]==l,])
+    for(l in 1:3){ #for each cluster
+      datarows <- data[mindistances[,2]==l,] #selects the data corresponding to each of the clusters
+      means[l,] <- colMeans(data[mindistances[,2]==l,])
+      #the above line takes the mean of each column and stores them as the new means vector
     }
   }
-  return(list(means, mindistances))
+  return(list(means, mindistances)) #returns means and mindistances
 }
 
-results <- cluster(mywine)
+sort_cluster <- function(datums){
 
-plotcluster(mywine, results[[2]][,2])
+results <- cluster(datums) #runs function defined above
 
-a <- results[[2]][1,2]
+
+
+a <- results[[2]][1,2] #creates a dummy variable "a" to first type of cluster
 b <- results[[2]][min(which(results[[2]][,2] != a)),2]
-c <- 6 - a - b
+#creates a dummy variable "b" to second type of cluster
+c <- 6 - a - b 
+#creates a dummy variable "c" to third type of cluster
 
+#the below block reassings the 1s and 2s to -1s and -2s in order to 
+#better compare with the established clustering that was removed from the data set
 results[[2]][results[[2]][,2] == a,2] <- -1
 results[[2]][results[[2]][,2] == b,2] <- -2
 results[[2]][results[[2]][,2] == c,2] <- 3
 results[[2]][results[[2]][,2] == -1,2] <- 1
 results[[2]][results[[2]][,2] == -2,2] <- 2
 
-sum(wine[,1] == results[[2]][,2])
+return(results) #returns results
 
+}
 
+results <- sort_cluster(mywine) 
+plotcluster(mywine, results[[2]][,2]) #plots clusters
+sum(wine[,1] == results[[2]][,2]) #checks to what degree our code and the
+#previous clustering actually agree
